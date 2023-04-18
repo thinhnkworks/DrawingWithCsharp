5cc2a3e3b80d4731033d88c791f896b56f8a2fc0
Thinh Nguyen <thinhnk.works@gmail.com>
2023-04-18 22:03:31 +0700

    Draw small ellipse

---
+++ GraphicsExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingWithC_
+{
+	public static class GraphicsExtension
+	{
+		private static float Height;
+
+		public static void SetParameters(this Graphics g, float height)
+		{
+			Height = height;
+		}
+
+		public static void SetTransform(this Graphics g)
+		{
+			g.PageUnit = GraphicsUnit.Millimeter;
+			g.TranslateTransform(0, Height);
+			g.ScaleTransform(1.0f, -1.0f);
+		}
+		public static void DrawPoint(this Graphics g, Pen pen, Entities.Point point)
+		{
+			g.SetTransform();
+			PointF p = point.Position.ToPointF;
+			g.DrawEllipse(pen, p.X - 1, p.Y - 1, 2, 2);
+			g.ResetTransform();
+		}
+	}
+}
---
+++ Vector3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingWithC_
+{
+	public class Vector3
+	{
+		private double x;
+		private double y;
+		private double z;
+
+		public Vector3(double x, double y)
+		{
+			this.X = x;
+			this.Y = y;
+			this.Z = 0.0;
+		}
+
+		public Vector3(double x, double y, double z) : this(x, y)
+		{
+			this.Z = z;
+		}
+
+
+		public double Z
+		{
+			get { return z; }
+			set { z = value; }
+		}
+
+		public double Y
+		{
+			get { return y; }
+			set { y = value; }
+		}
+
+		public double X
+		{
+			get { return x; }
+			set { x = value; }
+		}
+
+		public PointF ToPointF
+		{
+			get { return new PointF((float)X, (float)Y); }
+		}
+		public static Vector3 Zero
+		{
+			get { return new Vector3(0.0, 0.0, 0.0); }
+		}
+	}
+}
